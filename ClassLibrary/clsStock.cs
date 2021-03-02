b5618efcf1a39669762179ad589aaece7d0656cd
Yazz04 <P2528593@my365.dmu.ac.uk>
2021-03-02 19:43:42 +0000

    tested all of my attributes for the stock.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool LimitedStock { get; set; }
+        public int ProductID { get; set; }
+        public int ProductName { get; set; }
+        public int ProductType { get; set; }
+        public int Price { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
