2e42f4837c9eecd7ce72f8df31890e5accb05de1
Yazz04 <P2528593@my365.dmu.ac.uk>
2021-03-08 16:22:57 +0000

    Added all of the codes for testing my tblSchema. there are 8 test that I have just  procced. all codes are working fine with 0 errors.

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock Anstock = new clsStock();
+        //capture the product Name
+        Anstock.ProductName = txtProductName.Text;
+        //store the product Name in the session object
+        Session["Anstock"] = Anstock;
+        //navigate to the viewer page
+        Response.Redirect("StockViewer.aspx");
+    }
+
+    protected void txtPrice_TextChanged(object sender, EventArgs e)
     {
 
     }
--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsstock
+        clsStock Anstock = new clsStock();
+        //get the data from the session object
+        Anstock = (clsStock)Session["Anstock"];
+        //display the Product Name for the entry
+        Response.Write(Anstock.ProductName);
     }
 }
