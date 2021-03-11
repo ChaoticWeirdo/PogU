7d316498c0e164780a771e09207fff7d7a92f840
Addison Bur <P2408262@my365.dmu.ac.uk>
2021-03-11 01:50:05 +0000

    Created basic presentation layer for Data Entry. Connected Data Entry to Viewer. Also Changed some data types in both Orders class and test.

--- AdminSystem/OrdersViewer.aspx.cs
+++ AdminSystem/OrdersViewer.aspx.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of the class
+        clsOrders AnOrder = new clsOrders();
+        //get the data from the session object
+        AnOrder = (clsOrders)Session["AnOrder"];
+        //display the product id on the page
+        Response.Write(AnOrder.ProductId);
+        //display the description on the page
+        Response.Write(AnOrder.Description);
+        //display the status on the page
+        Response.Write(AnOrder.Status);
     }
 }
--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -6,11 +6,11 @@ namespace ClassLibrary
     {
         public int OrderId { get; set; }
         public int CustomerId { get; set; }
-        public int ProductId { get; set; }
+        public string ProductId { get; set; }
         public DateTime OrderDate { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
         public string Status { get; set; }
         public DateTime ShippedDate { get; set; }
+        public double Price { get; set; }
     }
 }
