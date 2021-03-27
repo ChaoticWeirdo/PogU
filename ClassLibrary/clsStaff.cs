32964d845cc7a18637583262862b8d02f04718d8
Emmanuel Eguasa <P2531208@my365.dmu.ac.uk>
2021-03-27 00:03:06 +0000

    Created the find method

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,19 +4,172 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
+        //private data member for the staff no property 
+        private Int32 mStaffID;
+        private DateTime mDateOfBirth;
+        private string mStaffFirstName;
+        private string mStaffLastName;
+        private string mGender;
+        private string mNINo;
+        private string mPhoneNo;
+        private string mAddress;
+        private string mPostCode;
+        private Boolean mCitizen;
+
         public clsStaff()
         {
         }
 
-        public bool Citizen { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public int StaffID { get; set; }
-        public string PostCode { get; set; }
-        public string Address { get; set; }
-        public string PhoneNo { get; set; }
-        public string NINo { get; set; }
-        public string Gender { get; set; }
-        public string StaffLastName { get; set; }
-        public string StaffFirstName { get; set; }
+        public bool Citizen
+        {
+            get
+            {
+                //return the private data 
+                return mCitizen;
+            }
+            set
+            {
+                //set the private data
+                mCitizen = value;
+            }
+        }
+        public DateTime DateOfBirth
+
+        {
+            get
+
+            {
+                //return the private data
+                return mDateOfBirth;
+            }
+            set
+            {
+                //the private data 
+                mDateOfBirth = value;
+
+            }
+
+        }
+        public Int32 StaffID
+        {
+            get
+            {
+                //this line of code sends data out of the property 
+                return mStaffID;
+            }
+            set
+            {
+                //this line of code allows data into the property 
+                mStaffID = value;
+            }
+        }
+        public string PostCode
+        {
+            get
+            {
+                //return the private data 
+                return mPostCode;
+            }
+            set
+            {
+                //set the private data
+                mPostCode = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                //return the private data 
+                return mAddress;
+            }
+            set
+            {
+                //set the private data
+                mAddress = value;
+            }
+        }
+        public string PhoneNo
+        {
+            get
+            {
+                //return the private data 
+                return mPhoneNo;
+            }
+            set
+            {
+                //set the private data
+                mPhoneNo = value;
+            }
+        }
+        public string NINo
+        {
+            get
+            {
+                //return the private data 
+                return mNINo;
+            }
+            set
+            {
+                //set the private data
+                mNINo = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                //return the private data 
+                return mGender;
+            }
+            set
+            {
+                //set the private data
+                mGender = value;
+            }
+        }
+        public string StaffLastName
+        {
+            get
+            {
+                //return the private data 
+                return mStaffLastName;
+            }
+            set
+            {
+                //set the private data
+                mStaffLastName = value;
+            }
+        }
+        public string StaffFirstName
+        {
+            get
+            {
+                //return the private data 
+                return mStaffFirstName;
+            }
+            set
+            {
+                //set the private data
+                mStaffFirstName = value;
+            }
+        }
+
+        public bool Find(int staffID)
+        {
+            //set the private data members to the test data value 
+            mStaffID = 21;
+            mDateOfBirth = Convert.ToDateTime("19/5/2014");
+            mStaffFirstName = "test name";
+            mStaffLastName = "test name";
+            mGender = "test Gender";
+            mNINo = "test NiNo";
+            mPhoneNo = "Test PhoneNo";
+            mAddress = "Test Address";
+            mPostCode = "Test Post Code";
+            mCitizen = true;
+            //always return true 
+            return true;
+        }
     }
 }
--- Testing1/tstStaff.cs
+++ Testing1/tstStaff.cs
@@ -160,5 +160,241 @@ namespace Testing1
 
         }
 
+        [TestMethod]
+        public void FindMethodOK()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //test to see if the result is true 
+            Assert.IsTrue(Found);
+
+        }
+
+        public void TestStaffNoFound()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the StaffID
+            if (AnStaff.StaffID != 21)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+
+        }
+
+        public void TestDateOfBirth()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.DateOfBirth != Convert.ToDateTime("19/05/2014"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void StaffFirstName()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.StaffFirstName != "Test name")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void StaffLastName()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.StaffLastName != "Test name")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void Gender()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.Gender != "Test Gender")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void PhoneNo()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.PhoneNo != "0758435638653")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void NINo()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.NINo != "Test Natiomal insurance number`")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+        public void Address()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.Address != "Chessnut grove")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void PostCode()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.PostCode != "XXX XXX")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
+
+        public void Citizen()
+
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //Boolean variable to store the results of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is )
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method 
+            Found = AnStaff.Find(StaffID);
+            //check the Property
+            if (AnStaff.Citizen != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+        }
     }
 }
