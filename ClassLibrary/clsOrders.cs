98fba5951d3c3cb48ec1f7b83743dedb54c095e3
Addison Bur <P2408262@my365.dmu.ac.uk>
2021-03-29 17:13:48 +0100

    Added Validation on the middle layer. Commented out OrderDate and DateShipped validation as coudnt get them to work

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -162,8 +162,137 @@ namespace ClassLibrary
             {
                 //return false indicating a problem
                 return false;
-            }
-            
+            }         
         }
+
+        public string Valid(string customerId, string productId, string orderDate, string description, string price, string status, string dateShipped)
+        {
+            //create a string variable to store the error
+            String Error = "";
+
+            //create a temporary variable to store datee values
+            DateTime DateTemp;
+
+            //if the Customer Id is blank
+            if (customerId.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Customer ID may not be blank : ";
+            }
+
+            //if the customer Id is greater than 6 characters
+            if (customerId.Length > 6)
+            {
+                //record the error
+                Error = Error + "The Customer ID must be less than 7 character : ";
+            }
+
+            //if Product Id is blank
+            if (productId.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Product ID may not be blank : ";
+            }
+
+            //if the Product Id is greater than 6 characters
+            if (productId.Length > 6)
+            {
+                //record the error
+                Error = Error + "The Product ID must be less that 7 characters : ";
+            }
+
+            /*
+            try
+            {
+                //copy the OrderDate value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(OrderDate);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                //check to see if the date is greater than today's date
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error = Error + "The date was not a valid date : ";
+            }*/
+
+            //if Description is blank
+            if (description.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Description may not be blank : ";
+            }
+
+            //if the Description is greater than 25 characters
+            if (description.Length > 25)
+            {
+                //record the error
+                Error = Error + "The Description must be less that 26 characters : ";
+            }
+
+            //if the Price is blank
+            if (price.Length == 0)
+            {
+                //record error
+                Error = Error + "The Price may not be blank : ";
+            }
+
+            //if the Price is greater than 7 characters
+            if (price.Length > 7)
+            {
+                //record the error
+                Error = Error + "The Price must be less than 8 characters : ";
+            }
+
+            //if the Status is blank
+            if (status.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Status may not be blank : ";
+            }
+
+            //if the Status is greater than 15 characters
+            if (status.Length > 15)
+            {
+                //record the error
+                Error = Error + "The Status must be less than 16 characters : ";
+            }
+
+            /*
+            try
+            {
+                //copy the DateShipped value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(DateShipped);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                //check to see if the date is greater than today's date
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                //record the error
+                Error = Error + "The date was not a valid date : ";
+            }
+            */
+
+            //return any error messages
+            return Error; 
+        }
+        
     }
 }
